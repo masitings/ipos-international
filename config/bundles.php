5a8d5eccb53d0733893a1846fa257dbdeb25ee76
Rafi Halilintar <masitingss@gmail.com>
2024-06-13 18:46:35 +0700

    Test TinyMCE

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,8 @@
 <?php
 
+use Pimcore\Bundle\TinymceBundle\PimcoreTinymceBundle;
+
 return [
     //Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    PimcoreTinymceBundle::class => ['all' => true],
 ];
