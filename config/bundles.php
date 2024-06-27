bb50924f59c06e2bdfdad02e74fa687551ffc3f2
Rafi Halilintar <masitingss@gmail.com>
2024-06-27 21:49:56 +0700

    Datahub

--- config/bundles.php
+++ config/bundles.php
@@ -1,8 +1,10 @@
 <?php
 
+use Pimcore\Bundle\DataHubBundle\PimcoreDataHubBundle;
 use Pimcore\Bundle\TinymceBundle\PimcoreTinymceBundle;
 
 return [
     //Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     PimcoreTinymceBundle::class => ['all' => true],
+    PimcoreDataHubBundle::class => ['all' => true],
 ];
