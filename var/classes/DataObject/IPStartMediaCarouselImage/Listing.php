525d573c4e4749d53e455528824570ffed983c7f
Waggyman <mas.awe.awe@gmail.com>
2025-02-27 04:43:42 +0000

    Add carousel more

---
+++ var/classes/DataObject/IPStartMediaCarouselImage/Listing.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Pimcore\Model\DataObject\IPStartMediaCarouselImage;
+
+use Pimcore\Model;
+use Pimcore\Model\DataObject;
+
+/**
+ * @method DataObject\IPStartMediaCarouselImage|false current()
+ * @method DataObject\IPStartMediaCarouselImage[] load()
+ * @method DataObject\IPStartMediaCarouselImage[] getData()
+ * @method DataObject\IPStartMediaCarouselImage[] getObjects()
+ */
+
+class Listing extends DataObject\Listing\Concrete
+{
+protected $classId = "32";
+protected $className = "IPStartMediaCarouselImage";
+
+
+/**
+* Filter by mediaTitle (Media Title)
+* @param string|int|float|array|Model\Element\ElementInterface $data  comparison data, can be scalar or array (if operator is e.g. "IN (?)")
+* @param string $operator  SQL comparison operator, e.g. =, <, >= etc. You can use "?" as placeholder, e.g. "IN (?)"
+* @return $this
+*/
+public function filterByMediaTitle ($data, $operator = '='): static
+{
+	$this->getClass()->getFieldDefinition("mediaTitle")->addListingFilter($this, $data, $operator);
+	return $this;
+}
+
+/**
+* Filter by mediaSubTitle (Media Sub Title)
+* @param string|int|float|array|Model\Element\ElementInterface $data  comparison data, can be scalar or array (if operator is e.g. "IN (?)")
+* @param string $operator  SQL comparison operator, e.g. =, <, >= etc. You can use "?" as placeholder, e.g. "IN (?)"
+* @return $this
+*/
+public function filterByMediaSubTitle ($data, $operator = '='): static
+{
+	$this->getClass()->getFieldDefinition("mediaSubTitle")->addListingFilter($this, $data, $operator);
+	return $this;
+}
+
+/**
+* Filter by mediaDescription (Media Description)
+* @param string|int|float|array|Model\Element\ElementInterface $data  comparison data, can be scalar or array (if operator is e.g. "IN (?)")
+* @param string $operator  SQL comparison operator, e.g. =, <, >= etc. You can use "?" as placeholder, e.g. "IN (?)"
+* @return $this
+*/
+public function filterByMediaDescription ($data, $operator = '='): static
+{
+	$this->getClass()->getFieldDefinition("mediaDescription")->addListingFilter($this, $data, $operator);
+	return $this;
+}
+
+/**
+* Filter by mediaThumbnail (Media Thumbnail)
+* @param string|int|float|array|Model\Element\ElementInterface $data  comparison data, can be scalar or array (if operator is e.g. "IN (?)")
+* @param string $operator  SQL comparison operator, e.g. =, <, >= etc. You can use "?" as placeholder, e.g. "IN (?)"
+* @return $this
+*/
+public function filterByMediaThumbnail ($data, $operator = '='): static
+{
+	$this->getClass()->getFieldDefinition("mediaThumbnail")->addListingFilter($this, $data, $operator);
+	return $this;
+}
+
+
+
+}
